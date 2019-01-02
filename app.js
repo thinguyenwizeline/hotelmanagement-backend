94626f9d049536e36f3b4d8d9aeb19eca1814c82
thinguyenwizeline <thi.nguyen@wizeline.com>
2019-01-02 22:14:45 +0700

    Fix bug res can not open

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 
-app.use((res, req, next) => {
+app.use((req, res, next) => {
     res.status(200).json({
         message: 'It works!'
     });
