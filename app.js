c3fee17717fefc6297e79d843f43d964ca1f5da2
thinguyenwizeline <thi.nguyen@wizeline.com>
2019-01-21 22:28:14 +0700

    Add error handle

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const roomRouters = require('./api/routes/rooms');
 const customerRouters = require('./api/routes/customers');
 
+app.use(morgan('dev'));
+
 app.use('/rooms',roomRouters);
 app.use('/customers',customerRouters);
+
+app.use((req,res,next) =>{
+    const error = new Error('Not found');
+    error.status(404);
+    next(error);
+});
 
 module.exports = app;
